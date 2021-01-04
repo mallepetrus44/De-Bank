767e9476715018e6a7d5cfd133c54935797a2883
Mathijs van de Kerkhof <mathijs.vandekerkhof@itvitaelearning.nl>
2021-01-04 13:43:41 +0100

    interface

---
+++ De Bank/Interfaces/IBank.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace De_Bank.Interfaces
+{
+   public interface IBank
+    {
+        //        Schrijf een applicatie die bank transacties simuleert, alle acties worden uitgevoerd door unit-testing.
+        //          De applicatie zal een Class Library Applicatie worden met als target framework.NET Core 3.1.
+        //          Gebruik code-first Enity-framework om je database te genereren.
+        //          De unittest zal de automatische betaling zal moeten starten, 4 keer laten uitvoeren en dan weer stoppen.
+
+
+
+        //De bank kan per account een overzicht geven van de transacties de afgelopen X seconden
+        public void GetTransactionBySeconds(int seconds, string account);
+
+
+        //          De bank kan per account een overzicht geven van alleen de positieve/negative transacties(van X sec.)
+        public void GetDebetCreditBySeconds(int seconds, string account);
+
+        //          De bank kan een overzicht opvragen van alle accounts gesorteerd op de voorkeur van de bank.
+        public void SortAccounts(string searchstring);
+
+        //          De bank kan een selectie maken van alle saldo’s die onder een X bedrag staan.
+        public void GetAmountsByAmount(double amount);
+
+        //          De bank kan automatische betalingen doen om de bijv. 30 sec.
+        public void DoAutoPayment();
+
+        //          De bank Kan het geld van de ene user naar de andere sturen.
+        public void SendMoney(double amount, string AccountA, string AccountB);
+
+        //          Deze transactie zal 5 seconden duren (gesimuleerd door een thread.sleep)
+        public void Sleep();
+
+        //          Er mogen meerdere users tegelijk van de zelfde rekening afschrijven. (Multi-Threading ).
+        public void MultiUserSending();
+
+        //          De transactie moet gecanceld en terug gedraaid worden als er te weinig saldo is.
+        public void ReTransact();
+    }
+
+
+}
--- De Bank/Logic/BankLogic.cs
+++ De Bank/Logic/BankLogic.cs
@@ -1,11 +1,72 @@
-﻿using System;
+﻿using De_Bank.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace De_Bank.Logic
 {
-    public class BankLogic
+    public class BankLogic : IBank
     {
+        //          De bank kan automatische betalingen doen om de bijv. 30 sec.
+        public void DoAutoPayment()
+        {
+            throw new NotImplementedException();
+        }
 
+
+        //          De bank kan een selectie maken van alle saldo’s die onder een X bedrag staan.
+        public void GetAmountsByAmount(double amount)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        //          De bank kan per account een overzicht geven van alleen de positieve/negative transacties(van X sec.)
+        public void GetDebetCreditBySeconds(int seconds, string account)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        //          De bank kan per account een overzicht geven van de transacties de afgelopen X seconden
+        public void GetTransactionBySeconds(int seconds, string account)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        //          Er mogen meerdere users tegelijk van de zelfde rekening afschrijven. (Multi-Threading ).
+        public void MultiUserSending()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        //          De transactie moet gecanceld en terug gedraaid worden als er te weinig saldo is.
+        public void ReTransact()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        //          De bank Kan het geld van de ene user naar de andere sturen.
+        public void SendMoney(double amount, string AccountA, string AccountB)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        //          Deze transactie zal 5 seconden duren (gesimuleerd door een thread.sleep)
+        public void Sleep()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        //          De bank kan een overzicht opvragen van alle accounts gesorteerd op de voorkeur van de bank.
+        public void SortAccounts(string searchstring)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
