e1dc3eee412779e7c27f8e6c2378d77be7ad124b
Luna Herder <luna@herderfactory.nl>
2021-01-07 12:22:13 +0100

    TestInitialize Data

--- Bank Test/BankLogicTest.cs
+++ Bank Test/BankLogicTest.cs
@@ -7,12 +7,41 @@ namespace Bank_Test
     [TestClass]
     public class BankLogicTest
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            var sut = new AccountHolder();
+        Account account;
+        AccountHolder accountHolder;
+        Transaction transaction;
 
-            Assert.IsNull(sut.AccountHolderName);
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            account = new Account
+            {
+                Id = 1,
+                AccountNumber = "102030",
+                AccountBalance = 1000.00,
+                AccountMinimum = 0.00,
+                //AccountHolder =   
+                //Transactions = 
+            };
+
+            accountHolder = new AccountHolder
+            {
+                Id = 1,
+                AccountHolderName = "John Wilder", 
+                //Accounts =
+            };
+
+            transaction = new Transaction 
+            { 
+                
+            };
+        }
+
+
+        [TestMethod]
+        public void CheckAcountHolderName()
+        {
+            Assert.AreEqual("John Wilder", accountHolder.AccountHolderName, true);
         }
     }
 }
