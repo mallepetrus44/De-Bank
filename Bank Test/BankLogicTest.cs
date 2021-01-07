7f706771d3104e93e6d83ce3e7648fbec90bed23
Luna Herder <luna@herderfactory.nl>
2021-01-07 11:40:48 +0100

    experimenting with unit testing

--- Bank Test/BankLogicTest.cs
+++ Bank Test/BankLogicTest.cs
@@ -1,13 +1,18 @@
+using De_Bank.Logic;
+using De_Bank.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Bank_Test
 {
     [TestClass]
-    public class UnitTest1
+    public class BankLogicTest
     {
         [TestMethod]
         public void TestMethod1()
         {
+            var sut = new AccountHolder();
+
+            Assert.IsNull(sut.AccountHolderName);
         }
     }
 }
