bdadd5d26a4f80c7acc22ce9f5a9a08c6f414699
Mathijs van de Kerkhof <mathijs.vandekerkhof@itvitaelearning.nl>
2021-01-12 02:43:43 +0100

    Identity + rename

---
+++ Bank.FrontEnd/Data/Migrations/20210112014312_accountholder to holder.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bank.FrontEnd.Data.Migrations
+{
+    public partial class accountholdertoholder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
